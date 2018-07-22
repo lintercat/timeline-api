a3353e3c4163aa6c237ec05d14310b8c0a61a189
Daniel Rejon <rejonpardenilla@gmail.com>
2018-07-22 17:42:22 -0500

    Add env variables

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :timeline, Timeline.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "timeline_dev",
+  username: System.get_env("POSTGRES_USERNAME"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  database: System.get_env("POSTGRES_DATABASE"),
   hostname: "localhost",
   pool_size: 10
