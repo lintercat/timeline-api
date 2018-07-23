811ae4a3fc1f0d47d68ad04a0a8356726d26698a
Daniel Rejon <rejonpardenilla@gmail.com>
2018-07-22 19:56:26 -0500

    Refactor project view serializer

--- lib/timeline_web/views/project_view.ex
+++ lib/timeline_web/views/project_view.ex
@@ -1,11 +1,11 @@
 defmodule TimelineWeb.ProjectView do
   use TimelineWeb, :view
   use JaSerializer.PhoenixView
-  alias Timeline.Work
+  alias TimelineWeb.ClientView
 
   attributes [:name, :client_id]
 
   has_one :client,
     include: true,
-    serializer: TimelineWeb.ClientView
+    serializer: ClientView
 end
